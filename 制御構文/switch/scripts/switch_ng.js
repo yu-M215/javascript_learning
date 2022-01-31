24a266e9b34f2dc9e792eb4baa07e8eb2359c3b7
yu-M215 <yuu0609km.215@gmail.com>
2022-01-31 12:44:34 +0900

    switchの比較について

---
+++ 制御構文/switch/scripts/switch_ng.js
@@ -0,0 +1,12 @@
+var x = '0';
+switch (x) {
+  case 0 :
+    console.log(x);
+    break;
+  case '0' :
+    console.log('データ型も揃えて比較する！');
+    break;
+  default :
+    console.log('デフォルト');
+    break;
+}
