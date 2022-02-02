ff4ba22e19a8705069641debf897089297f0553e
yu-M215 <yuu0609km.215@gmail.com>
2022-02-02 23:02:41 +0900

    フォルダの整理

---
+++ 基礎/scripts/array2.js
@@ -0,0 +1,2 @@
+var data = ['JavaScript', ['JQuery', 'prototype.js'], 'ASP.NET'];
+console.log(data[1][0]);
---
+++ 基礎/scripts/template.js
@@ -0,0 +1,5 @@
+let name = '鈴木';
+let str = `こんにちは、${name}さん。
+今日も良い天気ですね！`;
+
+console.log(str);
