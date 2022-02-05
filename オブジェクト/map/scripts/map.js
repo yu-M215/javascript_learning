d5a80f1c3bfddea7c291dd2811aa569a891683b4
yu-M215 <yuu0609km.215@gmail.com>
2022-02-05 22:13:34 +0900

    Mapオブジェクトで利用可能な主なメンバー

---
+++ オブジェクト/map/scripts/map.js
@@ -0,0 +1,32 @@
+// mapオブジェクトに値を追加
+let m = new Map();
+m.set('dog', 'ワンワン');
+m.set('cat', 'ニャー');
+m.set('mouse', 'チュー');
+
+console.log(m.size);
+console.log(m.get('dog'));
+console.log(m.has('cat'));
+
+// キーを順に取得
+for (let key of m.keys()) {
+  console.log(key);
+}
+
+// 値を順に取得
+for (let value of m.values()) {
+  console.log(value);
+}
+
+// キー／値を順に取得
+for (let [key, value] of m) {
+  console.log(value);
+}
+
+// キーdogを削除
+m.delete('dog');
+console.log(m.size);
+
+// すべてのキー／値を破壊
+m.clear();
+console.log(m.size);
