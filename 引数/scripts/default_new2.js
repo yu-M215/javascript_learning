ff817b98966dfdcdbaa22ea80f8f87216fa3c537
yu-M215 <yuu0609km.215@gmail.com>
2022-02-08 22:56:12 +0900

    デフォルト値を利用する際の注意点

---
+++ 引数/scripts/default_new2.js
@@ -0,0 +1,6 @@
+function multi(a, b = a) {
+  return a * b;
+}
+
+console.log(multi(10, 5));
+console.log(multi(3));
---
+++ 引数/scripts/default_new3.js
@@ -0,0 +1,5 @@
+function multi(a = blur, b = 5) {
+  return a * b;
+}
+
+console.log(multi());
