34dcba3355a92097dd54121807b652de974bc6c3
yu-M215 <yuu0609km.215@gmail.com>
2022-02-09 15:49:16 +0900

    プロトタイプオブジェクトにクラス共通で利用できるメソッドを定義す津

---
+++ プロトタイプ/scripts/prototype.js
@@ -0,0 +1,11 @@
+var Member = function(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+};
+
+Member.prototype.getName = function() {
+  return this.lastName + ' ' + this.firstName;
+};
+
+var mem = new Member('太郎', '山田');
+console.log(mem.getName());
