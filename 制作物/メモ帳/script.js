2128366fbaf06b8dc7067271fc3a6438bdfa998f
yu-M215 <yuu0609km.215@gmail.com>
2022-02-09 11:55:21 +0900

    メモ帳削除機能追加

--- 制作物/メモ帳/script.js
+++ 制作物/メモ帳/script.js
@@ -6,9 +6,9 @@ addBtn.addEventListener('click', () => addNewNote());
 
 function addNewNote(text = '') {
   // div要素を作成
-  const note = document.createElement('div')
+  const note = document.createElement('div');
   // noteクラスを追加
-  note.classList.add('note')
+  note.classList.add('note');
 
   // メモ帳を追加
   note.innerHTML = `
@@ -21,5 +21,28 @@ function addNewNote(text = '') {
   `
 
   // bodyの子要素として追加
-  document.body.appendChild(note)
+  document.body.appendChild(note);
+
+  // 操作に必要な要素を取得
+  const editBtn = note.querySelector('.edit');
+  const deleteBtn = note.querySelector('.delete');
+  const main = note.querySelector('textarea');
+
+  // 削除のクリックイベントの登録
+  deleteBtn.addEventListener('click', () => {
+    deleteNote(note)
+  })
+}
+
+// メモ帳削除
+function deleteNote(note) {
+  // ノートを削除
+  note.remove();
+
+  // ローカルストレージの更新
+  updateLS();
+}
+
+function updateLS() {
+
 }
