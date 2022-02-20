4c2a0c14bbe5b940710fd547213a42cc002a7ebe
yu-M215 <yuu0609km.215@gmail.com>
2022-02-20 23:38:40 +0900

    書式文字列を使ったログの表示

--- ブラウザーオブジェクト/scripts/log.js
+++ ブラウザーオブジェクト/scripts/log.js
@@ -2,3 +2,7 @@ console.log('ログ');
 console.info('情報');
 console.warn('警告');
 console.error('エラー');
+
+// 書式指定子を利用してログを出力する
+console.log('初めまして、私は %sです。%d 歳です。', '山田太郎', 30);
+console.log('今日の気温は、%.2f 度です。', 22.5);
